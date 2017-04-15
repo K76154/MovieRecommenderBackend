c7a6df879acfc1ca0a935a029fb503f40ec85fb1
JY Hsu <hga8561@seed.net.tw>
2017-04-15 15:57:31 +0800

    Basic UI skeleton

--- MovieRecommenderBackend/settings.py
+++ MovieRecommenderBackend/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'MovieRecommenderBackend.apps.URLDispatcher'
+    'MovieRecommenderBackend.apps.URLDispatcher',
+    'rest_framework_swagger'
 ]
 
 MIDDLEWARE = [
--- MovieRecommenderBackend/urls.py
+++ MovieRecommenderBackend/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from MovieRecommenderBackend.apps.URLDispatcher import views
+
+admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^login/$', auth_views.login),
+    url(r'^$', views.index)
 ]
